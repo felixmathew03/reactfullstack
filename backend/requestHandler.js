4b229cabe62f9051393460b8f3808f62dec63820
felixmathew03 <felixmathewtt@gmail.com>
2024-11-29 08:04:56 +0530

    e

--- backend/requestHandler.js
+++ backend/requestHandler.js
@@ -2,14 +2,9 @@ import todoSchema from "./models/todo.model.js";
 
 export async function addTodo(req,res){
     try{
-        console.log(req.body);
         const{...todo}=req.body;
-        
         const data=await todoSchema.create({...todo});
-       
-        
         return res.status(201).send({msg:"successfuly added"})
-        
     }catch(error){
         res.status(404).send({msg:"error"})
     }
@@ -17,11 +12,9 @@ export async function addTodo(req,res){
 
 export async function getTodos(req,res) {
     try {
-        console.log("fetch");
         const todos=await todoSchema.find();
         console.log(todos);
         res.status(200).send(todos)
-        
     } catch (error) {
         res.status(404).send({msg:"error"})
     }
@@ -31,8 +24,6 @@ export async function deleteTodo(req,res) {
     try {
          const {_id}=req.params;
         const data=await todoSchema.deleteOne({_id})
-        console.log("deleted");
-        
         res.status(201).send(data);
     } catch (error) {
         res.status(404).send(error)
