4f7e9b0abf13ab948578b10d525a428c3db30bce
felixmathew03 <felixmathewtt@gmail.com>
2024-12-17 08:53:51 +0530

    bbdhbvjb

--- backend/connection.js
+++ backend/connection.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 export default async function connection() {
-    const db= await mongoose.connect(process.env.DB_URL+process.env.DB_NAME)
+    const db= await mongoose.connect("mongodb+srv://xepake8350:RWPPxei4lNCPsDpQ@cluster0.ab2mv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     console.log("database connected");
     return db
 }
